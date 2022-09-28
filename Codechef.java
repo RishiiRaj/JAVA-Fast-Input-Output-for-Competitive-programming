b5b9735b464f6d73dc91f109a55a2a5c43a08b69
Rishi Raj <72983403+RishiiRaj@users.noreply.github.com>
2022-09-28 16:26:35 +0530

    Add files via upload

---
+++ Codechef.java
@@ -0,0 +1,92 @@
+import java.util.*;
+import java.io.*;
+
+class Codechef {
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine().trim();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	static class FastWriter {
+		private final BufferedWriter bw;
+
+		public FastWriter() {
+			this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		}
+
+		public void print(Object object) throws IOException {
+			bw.append("" + object);
+		}
+
+		public void println(Object object) throws IOException {
+			print(object);
+			bw.append("\n");
+		}
+
+		public void close() throws IOException {
+			bw.close();
+		}
+	}
+
+	// creating objects of input and output classes
+	public final static FastReader in = new FastReader();
+	public final static FastWriter out = new FastWriter();
+
+	public static void main(String[] args) {
+		try {
+			int testCases = in.nextInt();
+			while (testCases-- > 0) {
+				// write code here
+				Solve();
+			}
+			out.close();
+		} catch (Exception e) {
+			return;
+		}
+	}
+
+	public static void Solve() {
+		try {
+			// write code here
+		} catch (Exception e) {
+			return;
+		}
+	}
+}
